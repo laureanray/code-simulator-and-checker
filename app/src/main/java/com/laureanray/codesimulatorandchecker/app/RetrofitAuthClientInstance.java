d27bd5eb56c39a05abdc4472e7fc89a0c15551a1
laureanray <laureanraybahala@gmail.com>
2020-02-26 03:14:38 +0800

    Added fragment transition

---
+++ app/src/main/java/com/laureanray/codesimulatorandchecker/app/RetrofitAuthClientInstance.java
@@ -0,0 +1,20 @@
+package com.laureanray.codesimulatorandchecker.app;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitAuthClientInstance {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "http://192.168.100.47:3001";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
+}
